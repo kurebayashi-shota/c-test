95563fb1554bb6a640e1173ad822d352e6554a62
kurebayashi <shotakon77@gmail.com>
2025-11-10 17:53:14 +0900

    question5

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            question1();
+            question2();
+            question3();
+            question4();
+        }
+        static void question1()
+        {
+            Console.WriteLine("2.5.1.問題5-1のコンソール出力");
+            int[] array = new int[10];
+            try
+            {
+                Console.WriteLine("数値を入力してください");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(i + 1 + "回目の入力");
+                    array[i] = int.Parse(Console.ReadLine());
+                }
+            }
+            catch
+            {
+                Console.WriteLine("数値を入力しなさい");
+            }
+            foreach (int item in array) { Console.Write(item *2); };
+            Console.WriteLine("");
+        }
+        static void question2()
+        {
+            Console.WriteLine("2.5.2.問題5-2のコンソール出力");
+            int[] array = new int[10];
+            var evens = new List<int>();
+            var odds = new List<int>();
+            int i = 0;
+            try
+            {
+                Console.WriteLine("数値を入力してください");
+                for (i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(i + 1 + "回目の入力");
+                    array[i] = int.Parse(Console.ReadLine());
+                }
+            }
+            catch
+            {
+                Console.WriteLine("数値を入力しなさい");
+            }
+            for (i = 0; i < 10; i++) {
+                if (array[i] %2 == 0 ) { evens.Add(array[i]); }
+                else { odds.Add(array[i]); }
+            }
+
+            Console.Write("偶数:");
+            foreach(int even in evens)
+            {
+                Console.Write("{0}",even);
+            }
+            Console.WriteLine();
+            Console.Write("奇数:");
+            foreach (int odd in odds)
+            {
+                Console.Write("{0}", odd);
+            }
+            
+            Console.WriteLine("");
+        }
+        static void question3()
+        {
+        Console.WriteLine("2.5.3.問題5-3のコンソール出力");
+            int[,] kuku = new int[10, 10];
+            // 配列 kuku に値を代入する
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++){
+                kuku[i,j] = i*j;
+                Console.Write($":{i}×{j}={kuku[i, j]} ");
+                }
+            Console.WriteLine();
+            }
+            Console.WriteLine("");
+        }
+        static void question4()
+        {
+            Console.WriteLine("2.5.4.問題5-4のコンソール出力");
+            int[] array = new int[10];
+            try
+            {
+                Console.WriteLine("整数を入力してください");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(i + 1 + "回目の入力");
+                    array[i] = int.Parse(Console.ReadLine());
+                }
+                Array.Sort(array);
+            }
+            catch
+            {
+                Console.WriteLine("整数を入力しなさい");
+            }
+            Console.WriteLine("[{0}]", string.Join(", ", array));
+            ;
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //question1();
+            //question2();
+            //question3();
+            question4();
+        }
+        static void question1()
+        {
+            Console.WriteLine("2.6.1.問題6-1のコンソール出力");
+            static int Square(int input){return input*input;};
+            try
+            {
+                Console.WriteLine("数値を入力して下さい。");
+                int input = int.Parse(Console.ReadLine());
+                int result = Square(input);
+                Console.WriteLine($"入力した数値の2乗は{result}です。");
+            }
+            catch
+            {
+                Console.WriteLine("数値を入力しなさい");
+            }
+            Console.WriteLine("");
+        }
+        static void question2()
+        {
+            Console.WriteLine("2.6.2.問題6-2のコンソール出力");
+            static int Average(int input1 , int input2) { return (input1 + input2)/2; };
+            try
+            {
+                Console.WriteLine("数値1を入力して下さい。");
+                int input1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("数値2を入力して下さい。");
+                int input2 = int.Parse(Console.ReadLine());
+                int result = Average(input1, input2);
+                Console.WriteLine($"入力した数値の平均は{result}です。");
+            }
+            catch
+            {
+                Console.WriteLine("数値を入力しなさい");
+            }
+            Console.WriteLine("");
+        }
+        static void question3()
+        {
+            Console.WriteLine("2.6.3.問題6-3のコンソール出力");
+            static int LargerNum(int input1, int input2)
+            {
+                if(input1> input2)
+                {
+                    return input1;
+                }else
+                {
+                    return input2;
+                }
+            };
+            try
+            {
+                Console.WriteLine("数値1を入力して下さい。");
+                int input1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("数値2を入力して下さい。");
+                int input2 = int.Parse(Console.ReadLine());
+                if (input1 == input2)
+                {
+                    Console.WriteLine("入力した数値が同じ値の時の指定処理がありません。");
+                } else{
+
+                    int result = LargerNum(input1, input2);
+                    Console.WriteLine($"入力した大きい数値は{result}です。");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("数値を入力しなさい");
+            }
+            Console.WriteLine("");
+        }
+        static void question4()
+        {//エラー修正のみ
+            Console.WriteLine("2.6.4.問題6-4のコンソール出力");
+            static int Average(int[] array)
+            {
+                Console.WriteLine("平均。");
+                int average = 0;
+                int sum = 0;
+                for(int i=0; i < array.Length; i++) {
+                    sum = sum + array[i];
+                    average = sum / array.Length;
+                }
+                return average;
+            };
+            static int Max(int[] array)
+            {
+                Array.Sort(array);
+                int max = array[array.Length];
+                return max;
+            };
+            static int Min(int[] array)
+            {
+                Array.Sort(array);
+                int min = array[0];
+                return min;
+            };
+            int[] array = new int[5];
+            Console.WriteLine("数値を入力して下さい。");
+            try
+            {
+                for(int i=1; i<7; i++) { 
+                    Console.WriteLine($"{i}回目:");
+                    array[i-1] = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"{array[i - 1]}に格納しました。");
+                }
+                int max = Max(array);
+                int min = Min(array);
+                int average = Average(array);
+                Console.WriteLine($"入力された最大値は{max}です。");
+                Console.WriteLine($"入力された最小値は{min}です。");
+                Console.WriteLine($"入力された平均値は{average}です。");
+            }
+            catch
+            {
+                Console.WriteLine("数値を入力しなさい");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
